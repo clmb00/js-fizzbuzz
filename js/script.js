a6e446da606853f3a46fcc27a53081d09dd9f75f
clmb00 <paolo.colombo2000@gmail.com>
2022-10-13 15:53:27 +0200

    [ADD] script and basic style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('ciao')
+const container = document.querySelector(".container");
+for (let i = 1; i <= 100; i++){
+  const box = document.createElement('div');
+  box.classList = "box";
+  box.innerHTML = i;
+
+  if ((i%3==0) && (i%5==0)) {
+    box.innerHTML = "FizzBuzz";
+    box.classList += " bg-qui";
+  } else if (i%3 == 0) {
+    box.innerHTML = "Fizz";
+    box.classList += " bg-ter";
+  } else if (i%5 == 0) {
+    box.innerHTML = "Buzz";
+    box.classList += " bg-qua";
+  }
+
+  container.append(box);
+};
